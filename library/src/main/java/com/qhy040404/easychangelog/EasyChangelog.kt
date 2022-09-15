0e2a4fc9de785dc057151e996e59cb5c69157c40
qhy040404 <qhy040404@163.com>
2022-09-15 11:33:34 +0800

    Initial commit

---
+++ library/src/main/java/com/qhy040404/easychangelog/EasyChangelog.kt
@@ -0,0 +1,27 @@
+package com.qhy040404.easychangelog
+
+/**
+ * @param delimiter Delimiter between versions, e.g. "v", "---"
+ */
+class EasyChangelog(private val delimiter: String) {
+    /**
+     * @param fullLog Full Changelog
+     * @param version Version String
+     * @return current version's Changelog
+     */
+    fun parse(fullLog: String, version: String): List<String> {
+        val finalLogs = mutableListOf<String>()
+
+        val cut = fullLog.substringAfter(version).substringBefore(delimiter).trim()
+        cut.split("\n").forEach {
+            finalLogs.add(it.trim().extractItem())
+        }
+
+        return finalLogs
+    }
+
+    private fun String.extractItem(): String {
+        return if (this.startsWith("-") || this.startsWith("*")) this.substring(2)
+        else this
+    }
+}
